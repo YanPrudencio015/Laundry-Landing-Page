e25a2bc3fbe729b03fbab9258d56af760ca96ac4
Yan Pereira <yanpereira015@gmail.com>
2025-03-21 18:56:53 -0300

    changing design from Server Section

--- src/components/services/LoundryServices.jsx
+++ src/components/services/LoundryServices.jsx
@@ -1,34 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ServiceCardInfos } from "./informations/cardsInfo";
-              
+
+import imageTest from '../../images/Desktop/Lavagem-tapete.jpg'
+import iconTest from '../../icons/iron-board.png';
+
+
+
 
 function Services(props){
+
+
+      // Inicialize com os valores atuais da janela
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  const [sizesUpdated,setSizesUpdated] = useState({
+    widthUpdated: window.innerWidth,
+    heightUpdated: window.innerHeight
+
+  })
+
+  useEffect(() => {
+    // Função que atualiza o estado com o tamanho atual da janela
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    }
+    
+    // Adiciona o evento de escuta ao objeto window
+    window.addEventListener('resize', handleResize);
+    
+    // Limpa o evento quando o componente é desmontado
+    return () => {
+        window.removeEventListener('resize', handleResize);
+    };
+    
+}, []); // Array de dependências vazio significa que este efeito roda apenas uma vez na montagem
+
+
+useEffect(()=>{
+
+    setSizesUpdated({widthUpdated:windowSize.width,heightUpdated:windowSize.height})
+    console.log(sizesUpdated)
+
+},[windowSize])
+
     return(
         <div className='loundry-services-section'>
-        <div className='card-service-titles'>
-            <h1 className='card-service-title1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.<span>Serviços</span></h1>
-            <h6 className='card-service-title2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et error rerum velit ullam, omnis illum tempore neque.</h6>
-        </div>
-            {ServiceCardInfos.map((value, key)=>(
-                <div key={key} className='cards-Service'>
-                    <div className='front'>
-                        <div className='front-header'>
-                            <p className='front-title'>{value.frontTitle}</p>
-                        </div>
-                        <img src={value.icon} className='front-icon'/>
-                    </div>
-                        <div className='back'>
-                        <img className='back-img-BG' src={value.img[0]}></img>
-                        <div className="back-img-BG2"></div>
-                        <div className='back-text'>
-                            <h1 className='back-text-title'>{value.backTitle}</h1>
-                            <h6 className='back-text-desc'>{value.backDesc}</h6>
-                        </div>
-                    </div>
+            <div className="service-image-square">
+                <img src={imageTest} className="service-image"/>
+                <div className="service-image-square-BG"></div>
+                <p className="service-image-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus aperiam, soluta quis nemo non id, officia omnis exercitationem similique accusantium sint!
+                </p>
+            </div>
+            <div className="service-list-section">
+                <div className="service-list-header">
+                    <p className="service-list-header-Text">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        <span>Nossos Serviços</span>
+                    </p>
                 </div>
-            ))}
-          
-    </div>
+                <ul className="service-list-square">
+
+                        {ServiceCardInfos.map((value,key)=>(
+                            <li key={key} className="service-list-item">
+                                <div className="service-list-item-icon-BG">
+                                    <img src={value.icon} className="service-list-item-icon"/>
+                                </div>
+                                <div className="circle-BG"></div>
+                                <div className="service-list-item-text">
+                                <h2 className="service-list-item-title">{value.frontTitle}</h2>
+                                </div>
+                            </li>
+                        ))}
+                        <li className="bg-Service-Btn"
+                        ></li>
+                </ul>
+            </div>
+        </div>
 
     )
 }
