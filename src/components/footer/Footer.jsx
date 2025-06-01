e1e0264466cff7b33925ed724ea1d41365f929d9
YanPrudencio015 <yanpereira015@gmail.com>
2025-06-01 17:46:08 -0300

    creating footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,33 @@
 import React from "react";
-
-
+import './footer.css'
+import img from '../../images/wave-haikei.png'
 
 
 export default function Footer (props){
     return (
         <section className="footer-section">
             <div className="footer-section1">
-                <p className="ftr-sec1-title">Kilo Klin</p>
+                    <div className="footer-title-letters">
+                        <span className='footer-letter'>k</span>
+                        <span className='footer-letter'>i</span>
+                        <span className='footer-letter'>l</span>
+                        <span className='footer-letter'>o</span>
+                        <span className='footer-letter'>k</span>
+                        <span className='footer-letter'>l</span>
+                        <span className='footer-letter'>i</span>
+                        <span className='footer-letter'>n</span>
+                    </div>
+                    <div className="socials">
+                        <div className="icon-BG">
+                            <i class="fa-brands fa-facebook-f"></i>
+                        </div>
+                        <div className="icon-BG">
+                            <i class="fa-brands fa-whatsapp"></i>
+                        </div>
+                        <div className="icon-BG">
+                            <i class="fa-brands fa-instagram"></i>
+                        </div>
+                    </div>
             </div>
         </section>
     )
